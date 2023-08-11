3d2f3dc549015be16d276ac2f7bd223475f24882
idibek2002 <idibekrahimov0000@gmail.com>
2023-08-11 23:44:03 +0500

    fix problem

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ const resTimer = document.querySelector('.resTimer');
 const result__score = document.querySelector('.score__result');
 const result__final = document.querySelector('.result__final');
 const result__time = document.querySelector('.result__time');
-
-const emoji = ['😂', '😂', '😊', '😊', '😘', '😘', '😍', '😍', '❤', '❤', '🤣', '🤣', '😁', '😁', '😉', '😉', '😎', '😎', '😜', '😜'].sort(() => (Math.random() > 0.5) ? 2 : -1);
+const clickClass = document.querySelectorAll('.click');
+const emoji = ['😂', '😂', '😊', '😊', '😘', '😘', '😍', '😍', '❤', '❤', '🤣', '🤣', '😁', '😁', '🤢', '🤢', '😎', '😎', '😜', '😜'].sort(() => (Math.random() > 0.5) ? 2 : -1);
 let cnt = 0;
+let k = 0;
 let timer = 60;
 
 const get = () => {
@@ -17,22 +18,24 @@ const get = () => {
         block.innerHTML = item;
         block.onclick = () => {
             cnt++
+            k++
             if (cnt == 1) {
                 setInterval(() => {
                     if (timer > 0 && document.querySelectorAll('.success').length !== emoji.length) {
-                        resTimer.innerHTML = `00:${timer < 10 ? `0${--timer}` : --timer}`
+                        resTimer.innerHTML = `00:${timer < 11 ? `0${--timer}` : --timer}`
                     } else {
                         result__final.innerHTML = result.innerHTML
                         gameEl.classList.add('memory__hidden')
                         result__score.classList.add('res')
-                        result__time.innerHTML = `00:${timer < 10 ? `0${timer}` : timer}`
+                        result__time.innerHTML = `00:${timer < 11 ? `0${timer}` : timer}`
                     }
 
                 }, 1000)
             }
-            block.classList.add('click')
-            let clickClass = document.querySelectorAll('.click');
+            k <= 2 ? block.classList.add('click') : null
+            const clickClass = document.querySelectorAll('.click');
             setTimeout(() => {
+                clickClass.length == 2 ? k = 0 : null
                 if (clickClass.length > 1) {
                     if (clickClass[0].innerHTML === clickClass[1].innerHTML) {
                         clickClass[0].classList.add('success')
